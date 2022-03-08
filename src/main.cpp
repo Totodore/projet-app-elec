5d493befb8a29f5945dd69949c821565f509361e
azerty647601 <azerty647601@gmail.com>
2022-03-08 14:17:16 +0100

    push

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,37 @@
 #include <Arduino.h>
 #include "OledScreen.hpp"
 #include "data/logo.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-OledScreen screen;
+#define DHTPIN 2
+#define DHTTYPE    DHT11
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
 
 void setup()
 {
-	Serial.begin(9600);
-	Serial.println("Starting...");
-	Serial1.begin(9600);
-	Serial.println("Configuration ended");
-	screen.InitI2C();
-	screen.InitScreen();
-	screen.Reset();
-	screen.Display(logo);
-	screen.DisplayString(30, 71, "MyEcovillage");
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  sensor_t sensor;
+  Serial.println(F("------------------------------------"));
 }
-
-void loop()
+void loop() 
 {
-	if (Serial1.available()) {
-		Serial.write(Serial1.read());
-	}
-	if (Serial.available()) {
-		Serial1.write(Serial.read());
-	}
+	delay(1000);
+ 	sensors_event_t event;
+  	dht.temperature().getEvent(&event);
+
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature - 5);
+    Serial.println(F("°C"));
+
+	dht.humidity().getEvent(&event);
+	Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
 }
